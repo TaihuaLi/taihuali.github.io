f6192a8f5a8748b019b7f3668eab092df321c8d2
Taihua Li <taihua.ray.li@gmail.com>
2019-07-08 00:21:57 -0500

    remove last name

--- pages/email/send_email.php
+++ pages/email/send_email.php
@@ -11,7 +11,7 @@ $comments = $_REQUEST['message'] ;
 $first_name = $_REQUEST['fname'] ;
 $last_name = $_REQUEST['lname'];
 $msg =
-"Name: " . $first_name $last_name. "\r\n" .
+"Name: " . $first_name. "\r\n" .
 "Email: " . $email_address . "\r\n" .
 "Comments: " . $comments ;
 
